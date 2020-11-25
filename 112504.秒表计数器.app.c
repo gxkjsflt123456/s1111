c07d3ec88d1e3f0000a63c434725edfa9b5b7fe0
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-11-25 17:35:45 +0800

    Create 112504.秒表计数器.app.c

---
+++ 112504.秒表计数器.app.c
@@ -0,0 +1,11 @@
+# s1111
+#include<stdio.h>                                                                                               
+#include<windows.h>
+void main(){
+	int a;
+	printf("小程序");
+	Sleep(1000);
+		for(a=0;a>=0;a++){
+printf("%d",a);
+	}
+}
