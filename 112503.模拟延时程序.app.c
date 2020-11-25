dc0bd593a6cafb44720cf12e258b4fb0afb695ef
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-11-25 16:52:33 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,8 @@
+# s1111
+#include<stdio.h>                                                                                                                  
+#include<windows.h>
+void main(){
+printf("哇\n");
+Sleep(6000);
+printf(".....\n");
+}
