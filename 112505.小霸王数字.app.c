a3608d367830d8e09fca1a767b3931c3f0dbfbb9
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-11-26 11:04:51 +0800

    Create 112505.小霸王数字.app.c

---
+++ 112505.小霸王数字.app.c
@@ -0,0 +1,20 @@
+# s1111
+#include<stdio.h>
+#include<windows.h>
+void main(){ 
+int a=6;
+int b;
+scanf("%d",&b);
+if(a==b){
+printf("恭喜小主猜对了");
+}
+else if(a<b){
+printf("输入过大");
+}
+else{
+printf("输入过小");
+}
+
+
+
+}
