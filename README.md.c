a97fd22c2fb447cc564fd428a32ffe86944a75b5
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-12-02 16:43:20 +0800

    Create README.md.c

---
+++ README.md.c
@@ -0,0 +1,13 @@
+# s1111
+#include<stdio.h>
+void main(){
+int a,b,c,d;
+printf("请输入秒数");
+scanf("%d",&a);
+b=a/3600;
+c=(a%3600)/60;
+d=a-3600*b-60*c;
+printf("%d时：",b);
+printf("%d分：",c);
+printf("%d秒",d);
+}
