8781cafd1f22e82d21e68a2a847b51beafd5b896
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-12-03 22:12:30 +0800

    Create l1.c

---
+++ l1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+int a,b; 
+
+int c2d[20][10]={{999,999,999,999,999,999,999,999,999,999}};
+for(a=0;a<20;a++){
+for(b=0;b<10;b++)
+{ c2d[a][b]=999;
+Sleep(1000);
+c2d[a][b]=0;
+printf("%d ",c2d[a][b]);
+}
+}
+printf("%d ",c2d[a][b]);
+}
