2e78b67c5f4e92aba6eb5760d5e7db28a9a6d890
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-12-10 22:51:47 +0800

    Update 121001.猜数字.app.c

--- 121001.猜数字.app.c
+++ 121001.猜数字.app.c
@@ -1,22 +1,26 @@
-#include<stdio.h>
+        #include<stdio.h>
 #include<string.h>
 void main(){
 char c[60];
+char d[66]="其他";
 int a=6;
 int b;
-strcmp(c,"干饭")==0;
-scanf("%s",&c);
-printf("进入游戏");
-scanf("%d",&b);
- if(a==b){
-printf("你太厉害了");
+if(strcmp(c,"干饭")==0){
+	scanf("%s",&c);
+printf("进入游戏");}
+else if(strcmp(d,"其他")==0){
+scanf("%s",&d);
+	printf("请输入干饭");
 }
-else if(a<b){
+scanf("%d",&b);
+ if(a<b){
 printf("输入过大");
 }
 else if(a>b){
 printf("输入过小");}
-else{
+else if(a==b){
+printf("你太厉害了");
+}else{
 printf("输入错误");
 }
 }
