c930135226ad1f8d5fc574be1ac76d9e06144f67
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-12-10 20:51:22 +0800

    Create 121001.猜数字.app.c

---
+++ 121001.猜数字.app.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+char c[60];
+int a=6;
+int b;
+strcmp(c,"干饭")==0;
+scanf("%s",&c);
+printf("进入游戏");
+scanf("%d",&b);
+ if(a==b){
+printf("你太厉害了");
+}
+else if(a<b){
+printf("输入过大");
+}
+else if(a>b){
+printf("输入过小");}
+else{
+printf("输入错误");
+}
+}
