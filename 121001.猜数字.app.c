15e20b8d0c3a1b13a8b10546446612a9af609d84
gxkjsflt123456 <74294832+gxkjsflt123456@users.noreply.github.com>
2020-12-21 19:45:10 +0800

    Update 121001.猜数字.app.c

--- 121001.猜数字.app.c
+++ 121001.猜数字.app.c
@@ -3,20 +3,26 @@
 void main(){
 char c[60];
 char d[66]="其他";
-int a=6;
 int b;
+scanf("%s",&c);
 	if(strcmp(c,"干饭")==0){
-	scanf("%s",&c);
-printf("进入游戏");}
+	printf("进入游戏");
+while(b!=6){
+	scanf("%d",&b);
+	if(b>=7){
+		printf("输入过大");}
+	else if(b<=5){
+		printf("输入过小");}
+	else if(b==6){
+		printf("你太厉害了");}
+	else{
+		printf("输入错误");}
+	} }
 else if(strcmp(d,"其他")==0){
 	scanf("%s",&d);
 	printf("请输入干饭");
 }
-switch(0){
-	scanf("%d",&b);
-case 1 :printf("输入过大");
-case 2 :printf("输入过小");
-case 3 :printf("你太厉害了");break;
-case 4 :printf("输入错误");
+else{
+printf("1");
 }
 }
